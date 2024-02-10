fb6c13219a1a4d42409bf7c4dd12a643da54fe47
Kuba <jkalamaszek@gmail.com>
2024-02-10 16:01:04 +0100

    Add text in bonus.py

--- bonus/bonus.py
+++ bonus/bonus.py
@@ -10,6 +10,8 @@ filepaths = glob.glob("files/*.txt")
 # Create one PDF file
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
+
+
 for filepath in filepaths:
     # Create page
     pdf.add_page()
@@ -21,6 +23,15 @@ for filepath in filepaths:
     # Add the title to PDF
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, text=(header), new_x=XPos.LMARGIN, new_y=YPos.NEXT)
+
+    # Get text from files
+    with open(filepath, 'r') as file:
+        text = file.read()
+
+    # Add text to pdf
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, text=text)
+
 
 # Produce the PDF
 pdf.output("output.pdf")
